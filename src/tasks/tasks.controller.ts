95fd995e70d85fa63cf41bc96c1cf9fcd427edf0
unknown <afaizi1999@gmail.com>
2020-07-24 14:26:11 +0700

    Section 5: DONE

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,6 +10,7 @@ import {
   UsePipes,
   ValidationPipe,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TaskStatus } from './tasks-status.enum';
@@ -17,8 +18,10 @@ import { CreateTaskDTO } from './dto/create-task-dto';
 import { GetTaskFilterDTO } from './dto/get-task-filter-dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation-pipes';
 import { Task } from './tasks.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
